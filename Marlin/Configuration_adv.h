ba01fa1870b808f6d194250c69996af17ff4efcf
phillip <phillip.hagerman@scrith.com>
2024-04-16 21:08:17 -0500

    Removing UTF_FILENAME_SUPPORT

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1759,7 +1759,7 @@
 
   // Allow international symbols in long filenames. To display correctly, the
   // LCD's font must contain the characters. Check your selected LCD language.
-  #define UTF_FILENAME_SUPPORT
+  //#define UTF_FILENAME_SUPPORT
 
   #define LONG_FILENAME_HOST_SUPPORT      // Get the long filename of a file/folder with 'M33 <dosname>' and list long filenames with 'M20 L'
   //#define LONG_FILENAME_WRITE_SUPPORT   // Create / delete files with long filenames via M28, M30, and Binary Transfer Protocol
